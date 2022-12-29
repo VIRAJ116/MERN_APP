58d8af7118f444af76ee28b754cb7c12cefb45c6
YushaBinArif3 <virajraiyani162@gmail.com>
2022-12-29 15:55:31 +0530

    added build file to client

---
+++ client/build/precache-manifest.ef9c94ba9eb17f63b0d106d3752e87b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749572382b4ff363afeccf1b465f82aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83c98011202b2feb27bc",
+    "url": "/static/css/main.e8034bf1.chunk.css"
+  },
+  {
+    "revision": "abcdf0c2918392549c50",
+    "url": "/static/js/2.aaa69091.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.aaa69091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c98011202b2feb27bc",
+    "url": "/static/js/main.6d8c6594.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.6d8c6594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "8e9763ff2bd5c0571f6be83d85310d40",
+    "url": "/static/media/memoriesLogo.8e9763ff.png"
+  },
+  {
+    "revision": "6c7b5ec0184b26d4b93f36ec813156b3",
+    "url": "/static/media/memoriesText.6c7b5ec0.png"
+  }
+]);
